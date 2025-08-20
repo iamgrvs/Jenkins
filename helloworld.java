a0168ca2d3e2f01c1385a067179326c619383fcf
iamgrvs <48158924+iamgrvs@users.noreply.github.com>
2025-08-20 10:56:51 +0530

    helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
  class myworld {
     public static void main(String[] args) {
         System.out.println("Hello, batch 40 World! \n Soon I will get a good job with very high package\n60 LPA and more");
-     System.out.println("Hello Guys again");
+     System.out.println("Hello Guys again\nJust checking");
     }
 }
